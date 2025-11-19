72c1b8a7b86c74090be1cfbc19077fd7bd6190fb
arabi lioubi <l.loubi1886@uca.ac.ma>
2025-11-19 15:32:58 +0100

    add validation function for the employee modal

--- assets/index.js
+++ assets/index.js
@@ -1,12 +1,25 @@
 let formModal = document.getElementById("addEmployeeForm");
 
-formModal.addEventListener("submit", (e)=>{
-    e.preventDefault();
-    let inputs = formModal.querySelectorAll("input");
-    console.log(inputs)
+let regex = {
+    profileUrl : /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/,
+    name: /^[a-zA-Z\s]{3,}$/,
+    email: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    phone: /^\+212\s?[5-7]\d{8}$/,
+    
+}
+
+formModal.addEventListener("input", (e) => {
+    let input = e.target;
+    validate(input, regex[input.getAttribute("name")])
 })
 
 // function for validate the inputs
 function validate(input, regex) {
-    
+    if (regex.test(input.value)) {
+        input.style.borderColor = "#bdc3c7";
+        return true;
+    } else {
+        input.style.borderColor = "red";
+        return false;
+    }
 }
