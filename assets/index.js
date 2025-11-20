898caafe84e7048b94032de0783896dd77ef0310
arabi lioubi <l.loubi1886@uca.ac.ma>
2025-11-20 14:52:28 +0100

    updating employee info in the employeesArray

--- assets/index.js
+++ assets/index.js
@@ -56,7 +56,7 @@ function putItems() {
                             </svg>
                         </button>
 
-                        <button class="btn-icon btn-delete">
+                        <button class="btn-icon btn-delete" onclick="delete(${index})">
                             <svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor"
                                 stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                 <polyline points="3 6 5 6 21 6"></polyline>
@@ -174,19 +174,19 @@ function edit(index) {
     let employee = employeesArray[index];
 
     openEmployeeModal();
-    document.querySelector(".modal-header h2").innerText = "Edit: "+ employeesArray[index].name;
+    document.querySelector(".modal-header h2").innerText = "Edit: " + employeesArray[index].name;
 
     // fill the modal inputs
     let inputs = formModal.querySelectorAll(".photo-section input, .form-grid input, .form-grid select");
     document.getElementById("previewImg").src = employee.profileUrl;
-    inputs.forEach(input=>{
+    inputs.forEach(input => {
         input.value = employee[input.getAttribute("name")];
     })
     document.getElementById("experienceList").innerHTML = "";
     for (let i = 0; i < employee.experiences.length; i++) {
         document.getElementById("experienceList").innerHTML += `
                 <div class="experience-item">
-                                <button type="button" class="btn-remove-exp" style="display:${i? "block": "none"}">
+                                <button type="button" class="btn-remove-exp" style="display:${i ? "block" : "none"}">
                                     <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor"
                                         stroke-width="2">
                                         <line x1="18" y1="6" x2="6" y2="18"></line>
@@ -217,6 +217,54 @@ function edit(index) {
                                 </div>
                             </div>
             `;
-        
+
     }
+
+    formModal.addEventListener('sumbit', (e) => {
+        e.preventDefault();
+        
+        let updateEmployee = { experiences: [] }, isValid = true;
+
+        inputs.forEach(input => {
+            let name = input.getAttribute("name");
+            validate(input, regex[name]) ? (updateEmployee[name] = input.value) : (isValid = false);
+        });
+
+        //validating the experiences
+        formModal.querySelectorAll(".experience-item").forEach(exp => {
+            let newExp = {};
+            let inputs = exp.querySelectorAll("input");
+
+            validate(inputs[0], regex["exp_position"]) ? (newExp["exp_position"] = inputs[0].value) : (isValid = false);
+            validate(inputs[1], regex["exp_company"]) ? (newExp["exp_company"] = inputs[1].value) : (isValid = false);
+
+            // validating dates
+            let today = new Date();
+            let exp_start_date = new Date(inputs[2].value);
+            let exp_end_date = new Date(inputs[3].value);
+
+            if (exp_start_date < today && exp_end_date > exp_start_date) {
+                newExp["exp_start"] = inputs[2].value;
+                newExp["exp_end"] = inputs[3].value;
+            } else {
+                validate(inputs[2], false);
+                validate(inputs[3], false);
+                isValid = false;
+            }
+
+            updateEmployee["experiences"].push(newExp);
+        });
+
+        if (isValid) {
+            // update the unassigned list
+            employeesArray[index] = updateEmployee;
+            putItems();
+
+            closeEmployeeModal();
+        } else {
+            // show the warning message
+            document.getElementById("warningMessage").style.display = "block";
+        }
+
+    })
 }
